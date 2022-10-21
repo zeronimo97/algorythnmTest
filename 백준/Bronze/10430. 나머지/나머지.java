d407724a946d5ea9e0ffdbec9696b70c9cb36d3d
pablo <99312425+zeronimo97@users.noreply.github.com>
2022-10-21 15:01:14 +0900

    [Bronze V] Title: 나머지, Time: 208 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        int C= scan.nextInt();
+        scan.close();
+        System.out.println((A + B)%C);
+        System.out.println(((A % C) + (B%C))%C);
+        System.out.println((A * B)%C);
+        System.out.println(((A%C)*(B%C))%C);
+    }
+}
